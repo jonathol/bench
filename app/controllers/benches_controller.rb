0571a88ada1726ab1a202031c9b088cc113cac3f
App Academy Student <student@appacademy.io>
2016-06-24 11:04:49 -0700

    First Commit

---
+++ app/controllers/benches_controller.rb
@@ -0,0 +1,16 @@
+class BenchesController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def bench_params
+    params.require(:bench).permit(:description, :lat, :lng)
+  end
+
+end
